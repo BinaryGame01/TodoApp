e242825948aec23325c764497c6ad0edb109e278
Manish chaudhary <115458966+BinaryGame01@users.noreply.github.com>
2024-03-06 01:01:20 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,19 +1,17 @@
 const mongoose = require("mongoose");
-
 require("dotenv").config();
 
-const dbConnect = () => {
-    mongoose.connect(process.env.DATABASE_URL, {
-        useNewUrlParser:true,
-        useUnifiedTopology: true,
-    })
-    .then(() => console.log("DB ka Connection is Successful"))
-    .catch( (error) => {
-        console.log("Issue in DB Connection");
-        console.error(error.message);
-        //iska matlab kya h ?
+const dbConnect = async () => {
+    try {
+        await mongoose.connect(process.env.DATABASE_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("Database connection successful");
+    } catch (error) {
+        console.error("Error connecting to database:", error.message);
         process.exit(1);
-    } );
-}
+    }
+};
 
 module.exports = dbConnect;
